53903fd8d2b09ebec1fc8d8b4519cc0b63b685c0
optiverse.now <optiverse.now@gmail.com>
2025-01-12 12:49:17 +0900

    Add @dnd-kit/core dependency and implement drag-and-drop functionality in the page component; update layout language to Japanese

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+"use client";
+
+import { DndContext, DragEndEvent, UniqueIdentifier } from '@dnd-kit/core';
+import Droppable from '@/app/components/droppable';
+import Draggable from '@/app/components/draggable';
+import { useState } from 'react';
+
+
+export default function Page() {
+  const containers = ['A', 'B', 'C'];
+  const [parent, setParent] = useState<UniqueIdentifier | null>(null);
+
+  const draggableMarkup = (
+    <Draggable id="draggable">
+      <div className='cursor-grab w-48 h-20 bg-blue-200 flex justify-center items-center'>
+        Drag me
+      </div>
+    </Draggable>
   );
+  
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <div className='flex flex-col justify-center items-center w-screen h-screen gap-8'>
+        <div className='flex h-20'>
+          {parent === null ? draggableMarkup : null}
+        </div>
+        <div className='flex'>
+          {containers.map((id) => (
+            <Droppable key={id} id={id} isOverAddClass="bg-green-700">
+              <div className='w-52 h-24 border-2 border-dashed border-gray-100/50 flex justify-center items-center'>
+                {parent === id ? draggableMarkup : 'Drop here'}
+              </div>
+            </Droppable>
+          ))}
+        </div>
+      </div>
+    </DndContext>
+  );
+  
+  function handleDragEnd(event: DragEndEvent) {
+    const {over} = event;
+    setParent(over ? over.id : null);
+  }
 }
